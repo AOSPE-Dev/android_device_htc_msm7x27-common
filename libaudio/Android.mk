82fe5f4b8838f60fa3cc19e7df25f92547255f4a
GPE <owlprojectz@gmail.com>
2013-08-07 17:43:17 +0300

    Update Android.mk

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -27,7 +27,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.marvel
+LOCAL_MODULE := audio.primary
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES += libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
